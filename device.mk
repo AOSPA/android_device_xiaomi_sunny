c882ecd1e1e8f241450518b65c88aceeedd5e81f
Cyber Knight <cyberknight755@gmail.com>
2023-05-04 02:46:29 +0800

    sunny: Inherit wfd component
    - It's pretty minimal as we were using QTI WFD.
    
    Change-Id: I757638ed354c10e0035eea00f5c340268cc2f783
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -224,7 +224,8 @@ TARGET_COMMON_QTI_COMPONENTS += \
     perf \
     telephony \
     usb \
-    vibrator
+    vibrator \
+    wfd
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
@@ -301,9 +302,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libavservices_minijail_32 \
     libdisplayconfig.system.qti \
-    libnl \
     libqdMetaData.system \
     libwfdaac_vendor
 
