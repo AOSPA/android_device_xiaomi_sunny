8450a341cfa2c0e1b8aeef8dcbbde1e9f0b21b26
Cyber Knight <cyberknight755@gmail.com>
2023-05-10 11:59:16 +0800

    sunny: Drop DisplayFeatures
    - We don't need it anymore as we would transition to AOSP HBM and Automated DC Dimming.
    
    Change-Id: I73c1f34b53df06ef37c94784460f7ffa0c747e4d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -126,10 +126,6 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor
 
-# DisplayFeatures
-PRODUCT_PACKAGES += \
-    DisplayFeatures
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
