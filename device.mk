5cdf535db1f6b8d05e83537643315cbdbf52bb9c
Cyber Knight <cyberknight755@gmail.com>
2023-05-14 23:39:13 +0800

    sunny: Stop building camxoverridesettings.txt
    - We don't have it anymore.
    
    Change-Id: I976b3bc1755a6ee9008bc1dbd8217db4faeda3ff
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -78,8 +78,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libpiex_shim \
-    camxoverridesettings.txt
+    libpiex_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
