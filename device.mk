93db0f35179f99e6549b546dfa799888e228a187
Cyber Knight <cyberknight755@gmail.com>
2024-02-08 14:54:57 +0800

    sunny: Deduplicate FM packages
    - They're built at vendor/qcom/opensource/bluetooth/commonsys-intf.
    
    Change-Id: Id016ac2bac541d1ecaa7c7b936384b5436384190
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -125,11 +125,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    qcom.fmradio
-
 # GPS Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
